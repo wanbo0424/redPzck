921c05a853829f9971f75a1bb48a179cd75461bb
yinwb <yinwb@hytch.com>
2018-09-06 11:38:31 +0800

    toggle  IconSvg.vue

---
+++ demo-project/src/components/common/toggle.vue
@@ -0,0 +1,48 @@
+<template>
+     <div class="checkbox" :class="{active:toggle}">       
+        <div class="toggle" @click="toggle = !toggle" :class="{active:toggle}"></div> 
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      toggle: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.checkbox,
+.checkbox .toggle {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.checkbox {
+  width: 6rem;
+  height: 3.5rem;
+  background: linear-gradient(silver, whitesmoke);
+  border-radius: 2.5rem;
+  font-size: 40px;
+  &.active {
+    background: #3398ff
+  }
+  /* 按鈕 */
+  .toggle {
+    width: 3.5rem;
+    height: 3.5rem;
+    background: linear-gradient(to top, silver, whitesmoke);
+    border-radius: 50%;
+    box-shadow: 0 0.4rem 0.6rem rgba(0, 0, 0, 0.2);
+    position: relative;
+    left: -21%;
+    transition: left 0.5s ease;
+    &.active {
+      left: 21%;
+    }
+  }
+}
+</style>
